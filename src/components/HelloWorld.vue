4e1a1f42121d2d1630703b02578020730f4e373a
= <=>
2022-03-07 19:46:56 +0800

    done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,170 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-form-group
+            id="input-group-1"
+            label="Name:"
+            label-for="input-1"
+          >
+            <b-form-input
+              id="input-1"
+              v-model="form.name"
+              type="text"
+              required
+              placeholder="Enter Name"
+            >
+            </b-form-input>
+          </b-form-group>
+           <b-form-group
+            id="input-group-2"
+            label="E-mail:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.email"
+              type="email"
+              required
+              placeholder="Enter E-mail"
+            >
+            </b-form-input>
+          </b-form-group>
+
+          <template >
+            <b-button
+            v-show="form.update == false"
+             type="submit" class="my-3 w-100"
+            variant="primary"
+          >Submit</b-button>
+          
+          </template>
+          <b-button
+            v-show="form.update" @click="updateUser2" class="m-2"
+            variant="success"
+          >Done</b-button>
+          <b-button
+            v-show="form.update" @click="cancelUpdate" class="m-2"
+            variant="danger"
+          >Cancel</b-button>
+         
+        </b-form>
+      </b-col>
+      <b-col>
+        <b-table
+          striped hover :items="items" :fields="fields" :tbody-tr-class="rowClass"
+        >
+          <template #cell(actions)="row">
+            <b-button variant="success" size="sm" @click="updateUser(row.item.id)" class="mr-2">
+              Update
+            </b-button>
+            <b-button :disabled="form.update" variant="danger" size="sm" @click="deleteUser(row.item.id)" class="mr-1">
+              Delete
+            </b-button>
+          </template>
+        </b-table>  
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+          update:false
+        },
+        show: true,
+        items: [],
+        fields: [
+          {key:'name', label:'Name', sortable:true, sortDirection:'desc'},
+          {key:'email', label:'E-mail', class:'text-center'},
+          {key:'actions', label:'Actions'}
+        ],
+        con:0
+      }
+    },
+    methods: {
+      rowClass(item) {
+        if(!item.update) {
+          return 'table-light'
+        }
+        if(!item.update) {
+          return 'table-warning'
+        }
+      },
+      onSubmit(event) {
+        event.preventDefault()
+        this.items.push(
+          {
+            id:this.con,
+            name:this.form.name,
+            email:this.form.email,
+            update:false
+          }
+        )
+        this.con++
+        this.onReset()
+      },
+      onReset() {
+        // evt.preventDefault()
+        this.form.email = '',
+        this.form.name = '',
+        this.show = false,
+        this.$nextTick(()=>{
+          this.show = true
+        })
+      },
+        deleteUser(id) {
+        for(let index = 0; index < this.items.length; index++)
+        {
+          if(id == this.items[index].id)
+          {
+            this.items.splice(index, 1);
+          }
+        }
+      },
+      updateUser(id) {
+      for(let index = 0; index < this.items.length; index++) {
+        if(id == this.items[index].id)
+        {
+          this.form.email = this.items[index].email,
+          this.form.name = this.items[index].name,
+          this.form.update = true,
+          this.items[index].update = true
+        }
+      }
+    },
+    updateUser2() {
+      for(let index = 0; index < this.items.length; index++)
+      {
+        if(this.items[index].update) {
+          this.items[index].name = this.form.name,
+          this.items[index].email = this.form.email,
+          this.items[index].update = false,
+          this.form.email = '',
+          this.form.name = '',
+          this.form.update = false
+        }
+      }
+    },
+    cancelUpdate() {
+      this.form.email = '',
+      this.form.name = '',
+      this.form.update = false
+      for(let index = 0; index < this.items.length; index++)
+      {
+        this.items[index].update = false
+      }
+    }
+    }
   }
-}
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
